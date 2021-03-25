559c4b798dd1123ec3f47059fb8a2eadb7e4420e
DurbanD <durban.doyle@gmail.com>
2021-03-24 19:31:45 -0500

    Title & Meta tags

--- src/router/index.ts
+++ src/router/index.ts
@@ -8,7 +8,8 @@ const routes: Array<RouteConfig> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: { title: 'Durban Technology | OKC IT & Software Development' }
   },
   {
     path: '/about',
@@ -16,17 +17,46 @@ const routes: Array<RouteConfig> = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue'),
+    meta: { title: 'Durban Technology | About' }
   },
   {
     path: '/contact',
     name: 'Contact',
-    component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/Contact.vue'),
+    meta: { title: 'Durban Technology | Contact' }
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  const nearestWithTitle = to.matched.slice().reverse().find(r => r.meta && r.meta.title)
+  const nearestWithMeta = to.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+  const previousNearestWithMeta = from.matched.slice().reverse().find(r => r.meta && r.meta.metaTags)
+
+  if (nearestWithTitle) {
+    document.title = nearestWithTitle.meta.title
+  } else if (previousNearestWithMeta) document.title = previousNearestWithMeta.meta.title
+
+  Array.from(document.querySelectorAll('[data-vue-router-controlled]')).map(el => el.parentNode?.removeChild(el))
+
+  if (!nearestWithMeta) return next()
+
+  nearestWithMeta.meta.metaTags.map((tagDef: { [x: string]: string }) => {
+    const tag = document.createElement('meta')
+
+    Object.keys(tagDef).forEach(key => {
+      tag.setAttribute(key, tagDef[key])
+    })
+    tag.setAttribute('data-vue-router-controlled', '')
+
+    return tag
+  }).forEach((tag: any) => document.head.appendChild(tag))
+
+  next()
 })
 
 export default router
